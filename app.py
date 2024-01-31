173f024d76fd19eabb59842f40a53b822015070f
Sach <sach@dyn3165-45.wlan.ic.ac.uk>
2024-01-31 12:39:56 +0000

    working

--- app.py
+++ app.py
@@ -8,22 +8,6 @@ app = Flask(__name__)
 def index_page():
  return render_template("index.html")
 
-@app.route("/results", methods=['GET'])
-def results_page():
-    # Get the genre from the query parameters
-    genre = request.args.get('genre', '').lower()
-
-    # Make a request to the /consume route with the genre as a query parameter
-    response = requests.get(f'book-api-server-consume.dycqcecabah7djgd.uksouth.azurecontainer.io:5001/consume?genre={genre}')
-
-    if response.ok:
-        # If the request is successful, pass the JSON data to the template
-        books = response.json()
-        return render_template("results.html", books=books)
-    else:
-        # If there is an error, render an error template or handle it accordingly
-        return "Failed to retrieve data from the service", 500
-
 
 @app.route('/consume', methods=['GET'])
 def consume():
@@ -41,11 +25,11 @@ def consume():
         if genre:
             books = [book for book in books if genre in book.get('genre', '').lower()]
         if len(books) == 0:
-            return jsonify({"error": "No books found matching the criteria"}), 404
+            return render_template("results.html")
     else:
         return "Failed to retrieve data from the service", 500
 
-    return jsonify(books)
+    return render_template("results.html", books=books)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
